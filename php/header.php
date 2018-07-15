ba818e32d7ae468bf14294e85f7edfb22d10872b
Aubrey Cook <aubreyjcook@gmail.com>
2018-07-15 02:53:56 -0500

    importing current version of lampforge

---
+++ php/header.php
@@ -0,0 +1,11 @@
+<header class="grid-item row">
+  <div class="grid-item col-xs-12 col-sm-4 col-md-4 col-lg-4  center-sm center-md center-lg center-xs center-xs center-sm center-md center-lg">
+    <figure>
+      <img src="img/logo.jpg" alt="blue flower"></img>
+    </figure>
+    <figcaption>
+    Welcome to LampForge
+    </figcaption>
+  </div>
+  <?php include 'nav.php';?>
+</header>
---
+++ portfolio.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <?php include 'php/head-meta.php';?>
+        <?php include 'php/conditionalPageLoading.php';?>
+        <?php include 'php/head-links.php';?>
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <div class="container">
+          <?php include 'php/header.php';?>
+          <main class="grid-item row">
+            <?php include 'php/main-header.php';?>
+            <section class="grid-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+            <section class="grid-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+            <section class="grid-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+          </main>
+          <?php include 'php/footer.php';?>
+        </div>
+        <?php include 'php/body-scripts.php';?>
+    </body>
+</html>
